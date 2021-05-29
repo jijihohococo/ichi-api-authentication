84a8311d092a26bb8533031c770b4e0d20518134
suzume <johnstevemap@gmail.com>
2021-05-29 23:03:06 +0630

    add

--- database/migrations/2021_05_29_100000_create_api_authentications_table.php
+++ database/migrations/2021_05_29_100000_create_api_authentications_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateApiAuthenticationTable{
+    
 	public function up()
     {
         Schema::create('api_authentications', function (Blueprint $table) {
