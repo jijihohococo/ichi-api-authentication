47e04fc193f300fae9c55abace202a98d6c26ad0
suzume <johnstevemap@gmail.com>
2021-05-30 20:20:34 +0630

    fix database

--- database/migrations/2021_05_29_100000_create_ichi_api_authentications_table.php
+++ database/migrations/2021_05_29_100000_create_ichi_api_authentications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIchiApiAuthenticationTable extends Migration{
+class CreateIchiApiAuthenticationsTable extends Migration{
 
     protected $schema;
 
