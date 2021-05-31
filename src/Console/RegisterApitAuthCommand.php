8c23639e574c396ce8ca005fef8c5e453e422418
suzume <johnstevemap@gmail.com>
2021-05-31 12:13:31 +0630

    fix

--- src/Console/RegisterApitAuthCommand.php
+++ src/Console/RegisterApitAuthCommand.php
@@ -5,6 +5,13 @@ use Illuminate\Console\Command;
 class RegisterApiAuthCommand extends Command{
 
 	/**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature ="";
+
+	/**
      * The console command description.
      *
      * @var string
@@ -12,7 +19,7 @@ class RegisterApiAuthCommand extends Command{
     protected $description = 'Create Provider with guard to access api tokens';
 
     public function handle(){
-    	
+
     }
 
 }
