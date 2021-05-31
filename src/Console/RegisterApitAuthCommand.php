009ff376926b2d17ea45cff6c793a6a3c1c11454
suzume <johnstevemap@gmail.com>
2021-05-31 12:11:26 +0630

    fix

--- src/Console/RegisterApitAuthCommand.php
+++ src/Console/RegisterApitAuthCommand.php
@@ -4,4 +4,15 @@ namespace JiJiHoHoCoCo\IchiApiAuthentication\Console;
 use Illuminate\Console\Command;
 class RegisterApiAuthCommand extends Command{
 
+	/**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create Provider with guard to access api tokens';
+
+    public function handle(){
+    	
+    }
+
 }
