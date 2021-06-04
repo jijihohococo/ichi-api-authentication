bd0140fb5ec4c5875c2afacf98bcab463fd2e653
suzume <johnstevemap@gmail.com>
2021-06-05 05:02:50 +0630

    add

--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -8,7 +8,7 @@ class ClientRepository{
 	return	IchiApiAuthentication::create([
 			'guard_name' => $guard ,
 			'model_name' => config('auth.providers.'. 
-			\Arr::get($selectedGuardArray , 'user_api.provider' ) .'.model')
+			\Arr::get($selectedGuardArray , $guard '.provider' ) .'.model')
 		]);
 	}
 	
