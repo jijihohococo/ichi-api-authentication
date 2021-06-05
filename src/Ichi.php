c2614df1cedd6735836aa05722053c8994ae0fd9
suzume <johnstevemap@gmail.com>
2021-06-05 17:49:50 +0630

    fix

--- src/Ichi.php
+++ src/Ichi.php
@@ -3,7 +3,7 @@
 namespace JiJiHoHoCoCo\IchiApiAuthentication;
 use Carbon\Carbon;
 class Ichi{
-	public $expired_at = Carbon::now();
+	public $expired_at = Carbon::now()->addDays(5);
 
 	public function setExpiredAt($expiredDate){
 		$this->expired_at=$expiredDate;
--- src/Repository/TokenRepository.php
+++ src/Repository/TokenRepository.php
@@ -12,18 +12,18 @@ class TokenRepository{
 
 	public static function updateOrCreate($guard,$userId){
 		
-			$apiId=self::getApiId($guard);
-		
-				return IchiTokenAuthentication::updateOrCreate(
-					['user_id'=>$userId , 'api_authentication_id' => $apiId ],
-					[
-						'user_id' => $userId ,
-						'token' => Hash::make($guard. $userId . time()) ,
-						'expired_at' => "date",
-						'api_authentication_id' => $apiId,
-						'revoke' => false
-					]);
-			
+		$apiId=self::getApiId($guard);
+		$ichi=new Ichi;
+		IchiTokenAuthentication::updateOrCreate(
+			['user_id'=>$userId , 'api_authentication_id' => $apiId ],
+			[
+				'user_id' => $userId ,
+				'token' => Hash::make($guard. $userId . time()) ,
+				'expired_at' => $ichi->getExpiredAt()  ,
+				'api_authentication_id' => $apiId,
+				'revoke' => false
+			]);
+
 	}
 
 	public function revoke($id){
