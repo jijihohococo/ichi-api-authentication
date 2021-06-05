22939572f7e3bc17c2c838e240638e739febf5c4
suzume <johnstevemap@gmail.com>
2021-06-05 19:43:20 +0630

    add

--- src/Ichi.php
+++ src/Ichi.php
@@ -3,13 +3,13 @@
 namespace JiJiHoHoCoCo\IchiApiAuthentication;
 use Carbon\Carbon;
 class Ichi{
-	public $expired_at = Carbon::now()->addDays(5);
+	public $expired_at;
 
 	public function setExpiredAt($expiredDate){
 		$this->expired_at=$expiredDate;
 	}
 
 	public function getExpiredAt(){
-		return $this->expired_at;
+		return $this->expired_at==null ? Carbon::now()->addDays(5) : $this->expired_at ;
 	}
 }
