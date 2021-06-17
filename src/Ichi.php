ede641a9dccd8c88088b892009c970b8eddf2816
suzume <johnstevemap@gmail.com>
2021-06-17 14:59:01 +0630

    fix

--- src/Ichi.php
+++ src/Ichi.php
@@ -2,11 +2,11 @@
 
 namespace JiJiHoHoCoCo\IchiApiAuthentication;
 use Carbon\Carbon;
-use DateTimeInterface;
+use DateInterval;
 class Ichi{
 	public static $expired_at;
 
-	public static function setExpiredAt(DateTimeInterface $date=null){
+	public static function setExpiredAt(DateInterval $date=null){
 		if($date==null){
 			return	static::$expired_at !== null ?  Carbon::now()->diff(static::$expired_at ) :  getStandardExpired();
 		}
