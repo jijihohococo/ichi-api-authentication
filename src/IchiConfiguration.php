fdb58b3108c7251430287aeae616a06f05962660
suzume <johnstevemap@gmail.com>
2021-06-17 17:48:10 +0630

    fix

--- src/IchiConfiguration.php
+++ src/IchiConfiguration.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace JiJiHoHoCoCo\IchiApiAuthentication;
-use DateInterval;
+use DateInterval,DateTime;
 class IchiConfiguration{
 	public $expiredAt;
 	public function setExpiredAt(DateInterval $date){
-		$currentDate=date("Y-m-d H:i:s");
+		$currentDate=new DateTime(date("Y-m-d H:i:s"));
 		$currentDate->add($date);
 		$this->expiredAt=$currentDate->format('Y-m-d H:i:s');
 	}
