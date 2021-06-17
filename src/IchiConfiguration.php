f3325dfaeba8ef142ad9f6b9d8053c1706441cae
suzume <johnstevemap@gmail.com>
2021-06-17 16:57:45 +0630

    fix

--- src/IchiConfiguration.php
+++ src/IchiConfiguration.php
@@ -5,7 +5,7 @@ use DateInterval;
 class IchiConfiguration{
 	public $expiredAt;
 	public function setExpiredAt(DateInterval $date){
-		$this->expiredAt=$date->format("P%yY%mM%dDT%hH%iM%sS");
+		$this->expiredAt=$date->format('Y-m-d H:i:s');
 	}
 
 	public function getExpiredAt(){
